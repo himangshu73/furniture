5ad6ac57fc2e8cc9527f71fa9b82c8480ab7469b
himangshu <himangshu73@gmail.com>
2025-07-27 23:05:45 +0600

    nav basic

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+export default function () {
+  return (
+    <div className="container mx-auto px-8 py-4">
+      <nav className="flex justify-between">
+        <Image src="/logo.png" width={50} height={33} alt="logo" />
+        <div>
+          <RxHamburgerMenu size={33} />
+        </div>
+      </nav>
+    </div>
+  );
+}
