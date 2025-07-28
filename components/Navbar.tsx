7d9a932ebc1e63092974e0015f62fe0cb483304c
himangshu <himangshu73@gmail.com>
2025-07-28 23:31:16 +0600

    logo type

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,11 @@ export default function Navbar() {
   return (
     <div className="container mx-auto px-8 py-4">
       <nav className="flex justify-between">
-        <Image src="/logo.png" width={50} height={33} alt="logo" />
+        <div className="relative w-[50px] h-[32px]">
+          <Image src="/logo.svg" fill alt="logo" className="object-contain" />
+        </div>
         <div>
-          <RxHamburgerMenu size={33} />
+          <RxHamburgerMenu size={32} />
         </div>
       </nav>
     </div>
