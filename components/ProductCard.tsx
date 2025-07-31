97a88931f3d8b11104e70bb41c2c8c46b5b763a5
himangshu <himangshu73@gmail.com>
2025-08-01 00:19:11 +0600

    product card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,33 +1,41 @@
 import Image from "next/image";
 
-export default function ProductCard(){
-    return(
-        <div className="relative w-[250px]">
-          <div>
-            <div className="relative w-[250px] h-[300px]">
-              <Image
-                src="/image1.jpg"
-                fill
-                alt="image"
-                className="object-cover"
-              />
-            </div>
-            <div className="bg-orange-100 flex flex-col px-2 py-4 gap-2">
-              <h2 className="text-xl font-bold">Syltherine</h2>
-              <p className="text-base text-gray-600">Stylish cafe chair</p>
-              <div className="flex gap-4">
-                <h3 className="text-lg font-semibold">Rp 2.500.000</h3>
-                <h4 className="text-base text-gray-400 line-through">
-                  Rp 3.500.000
-                </h4>
-              </div>
-            </div>
-          </div>
-          <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 flex items-center justify-center transition-opacity duration-300">
-            <button className="bg-white px-8 py-2 text-orange-400 text-sm">
-              Add to cart
-            </button>
+interface Props {
+  image: string;
+  name: string;
+  description: string;
+  price: number;
+  standardPrice: number;
+}
+
+export default function ProductCard(props: Props) {
+  return (
+    <div className="relative w-[250px]">
+      <div>
+        <div className="relative w-[250px] h-[300px]">
+          <Image
+            src={props.image}
+            fill
+            alt={props.name}
+            className="object-cover"
+          />
+        </div>
+        <div className="bg-orange-100 flex flex-col px-2 py-4 gap-2">
+          <h2 className="text-xl font-bold">{props.name}</h2>
+          <p className="text-base text-gray-600">{props.description}</p>
+          <div className="flex gap-4">
+            <h3 className="text-lg font-semibold">Rp {props.price}</h3>
+            <h4 className="text-base text-gray-400 line-through">
+              Rp {props.standardPrice}
+            </h4>
           </div>
         </div>
-    )
+      </div>
+      <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 flex items-center justify-center transition-opacity duration-300">
+        <button className="bg-white px-8 py-2 text-orange-400 text-sm">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
 }
--- components/Products.tsx
+++ components/Products.tsx
@@ -5,9 +5,27 @@ export default function Products() {
     <div>
       <div className="flex flex-col items-center py-8 gap-4">
         <h2 className="text-3xl font-bold">Our Products</h2>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <ProductCard
+          image="/image1.jpg"
+          name="Himangshu"
+          description="Stylish cafe chair"
+          price={2200.0}
+          standardPrice={3200.0}
+        />
+        <ProductCard
+          image="/image1.jpg"
+          name="Suman Baba"
+          description="Stylish cafe chair"
+          price={5000.0}
+          standardPrice={8000.0}
+        />
+        <ProductCard
+          image="/image1.jpg"
+          name="Barmon"
+          description="Stylish cafe chair"
+          price={4000.0}
+          standardPrice={7000.0}
+        />
       </div>
     </div>
   );
