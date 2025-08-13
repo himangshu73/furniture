e25f7c2f908895cf1dc3e2ca37204418bae178d3
himangshu <himangshu73@gmail.com>
2025-08-13 23:39:36 +0600

    menubar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,19 +13,28 @@ export default function Navbar() {
         <div className="relative w-[50px] h-[32px]">
           <Image src="/logo.svg" fill alt="logo" className="object-contain" />
         </div>
+        <div className="flex gap-4">
+          <p className="text-lg cursor-pointer text-gray-900 hover:text-gray-700">
+            About
+          </p>
+          <p className="text-lg cursor-pointer text-gray-900 hover:text-gray-700">
+            Contact
+          </p>
+          <p className="text-lg cursor-pointer text-gray-900 hover:text-gray-700">
+            Privacy
+          </p>
+        </div>
         <button
           title="menu"
           aria-label="Open Menu"
           onClick={() => setMenuOpen(!menuOpen)}
+          className="md:hidden"
         >
           {menuOpen ? <IoMdClose size={32} /> : <RxHamburgerMenu size={32} />}
         </button>
       </nav>
       {menuOpen && (
-        <ul className="absolute z-50 right-4 p-4 rounded-xl shadow-lg bg-orange-200 mt-2 w-40 space-y-2 text-center transition duration-300">
-          <li className="hover:bg-orange-300 rounded-lg cursor-pointer px-4 py-2">
-            Home
-          </li>
+        <ul className="md:hidden absolute z-50 right-4 p-4 rounded-xl shadow-lg bg-orange-200 mt-2 w-40 space-y-2 text-center transition duration-300">
           <li className="hover:bg-orange-300 rounded-lg cursor-pointer px-4 py-2">
             About
           </li>
