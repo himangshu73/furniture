102a0a4e045a1f668ca0f1fd6a82b85937d13292
himangshu <himangshu73@gmail.com>
2025-08-17 22:31:32 +0600

    footer complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,16 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="container mx-auto px-8 py-4">
+      <hr className="w-full border-gray-300 mt-8 mb-8" />
       <div className="flex flex-col gap-8 md:gap-2 md:flex-row justify-between">
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col gap-8">
           <h1 className="text-2xl font-bold">Furniro</h1>
-          <p>400 University Drive Suite 200 Coral Gables, FL 33134 USA</p>
+          <p className="text-gray-400">
+            400 University Drive Suite 200 Coral Gables, <br />
+            FL 33134 USA
+          </p>
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-8">
           <p className="text-gray-400 font-semibold">Link</p>
           <div className="flex flex-col gap-4">
             <Link href="" className="font-semibold">
@@ -25,7 +29,7 @@ export default function Footer() {
             </Link>
           </div>
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-8">
           <p className="text-gray-400 font-semibold">Help</p>
           <Link href="" className="font-semibold">
             Payment Options
@@ -37,8 +41,20 @@ export default function Footer() {
             Priacy Policy
           </Link>
         </div>
-        <div>Newsletter</div>
+        <div className="flex flex-col gap-8">
+          <p className="text-gray-400 font-semibold">Newsletter</p>
+          <div className="flex gap-2">
+            <input
+              type="text"
+              placeholder="Enter Your Email Address"
+              className="px-2 py-3 underline"
+            />
+            <button className="underline cursor-pointer">SUBSCRIBE</button>
+          </div>
+        </div>
       </div>
+      <hr className="w-full border-gray-300 mt-8 mb-8" />
+      <div>{new Date().getFullYear()} Furniro. All rights reserved.</div>
     </div>
   );
 }
