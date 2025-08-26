3c5a7a9b31c554c5174b3d84daa6f45a8deee8d4
himangshu <himangshu73@gmail.com>
2025-08-27 00:16:07 +0600

    mobile center

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Shop() {
   return (
     <div className="container mx-auto px-8 py-16 bg-orange-100">
-      <div className="flex md:flex-row flex-col gap-4 items-center justify-between">
+      <div className="flex md:flex-row flex-col gap-4 justify-between">
         <div className="flex gap-2">
           <Image src="trophy.svg" width={40} height={40} alt="trophy" />
           <div>
