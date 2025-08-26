cbce540e31ae5648d6a83e61dd631514da327d6a
himangshu <himangshu73@gmail.com>
2025-08-27 00:10:40 +0600

    cc

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -2,33 +2,34 @@ import Image from "next/image";
 
 export default function Shop() {
   return (
-    <div className="container mx-auto px-8 py-16 bg-red-100">
+    <div className="container mx-auto px-8 py-16 bg-orange-100">
       <div className="flex md:flex-row flex-col gap-4 items-center justify-between">
-        <div>
-          <div className="flex gap-2">
-            <Image src="trophy.svg" width={40} height={40} alt="trophy" />
-            <div>
-              <h2 className="text-xl font-semibold">High Quality</h2>
-              <p>Crafted from top materials</p>
-            </div>
+        <div className="flex gap-2">
+          <Image src="trophy.svg" width={40} height={40} alt="trophy" />
+          <div>
+            <h2 className="text-lg font-semibold">High Quality</h2>
+            <p className="text-gray-500">Crafted from top materials</p>
           </div>
         </div>
-        <div>
+        <div className="flex gap-2">
+          <Image src="warranty.svg" width={40} height={40} alt="warranty" />
           <div>
-            <h2 className="text-xl font-semibold">Warranty Protection</h2>
-            <p>Over 2 years</p>
+            <h2 className="text-lg font-semibold">Warranty Protection</h2>
+            <p className="text-gray-500">Over 2 years</p>
           </div>
         </div>
-        <div>
+        <div className="flex gap-2">
+          <Image src="shipping.svg" width={40} height={40} alt="shipping" />
           <div>
-            <h2 className="text-xl font-semibold">Free Shipping</h2>
-            <p>Order over 150 $</p>
+            <h2 className="text-lg font-semibold">Free Shipping</h2>
+            <p className="text-gray-500">Order over 150 $</p>
           </div>
         </div>
-        <div>
+        <div className="flex gap-2">
+          <Image src="cc.svg" width={40} height={40} alt="customer" />
           <div>
-            <h2 className="text-xl font-semibold">24 / 7 Support</h2>
-            <p>Dedicated support</p>
+            <h2 className="text-lg font-semibold">24 / 7 Support</h2>
+            <p className="text-gray-500">Dedicated support</p>
           </div>
         </div>
       </div>
